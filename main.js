69981f045b717d67b228fd93d6e486d75100397e
João Victor Carvalho <silva.joaovictorcarvalho@gmail.com>
2025-04-22 21:42:42 -0300

    Implementa criação do corpo da tabela

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ document.getElementById('adicionarGasto').addEventListener('click', () => {
     const valor = parseFloat(valorInput.value);
     const descricao = descricaoInput.value;
     const categoria = categoriaSelect.value;
+
+    if (!isNaN(valor) && valor > 0 && descricao.trim() !== '') {
+        totalGastos += valor;
+        document.getElementById('totalGastos').textContent = `Total: R$ ${totalGastos.toFixed(2)}`;
+
+       
+        const tabelaBody = document.getElementById('tabelaGastos').getElementsByTagName('tbody')[0];
+        const novaLinha = tabelaBody.insertRow();
+
+        const celulaCategoria = novaLinha.insertCell(0);
+        const celulaDescricao = novaLinha.insertCell(1);
+        const celulaValor = novaLinha.insertCell(2);
+        const celulaExcluir = novaLinha.insertCell(3);  
+
+        celulaCategoria.textContent = categoria;
+        celulaDescricao.textContent = descricao;
+        celulaValor.textContent = `R$ ${valor.toFixed(2)}`;
